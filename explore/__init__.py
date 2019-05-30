da0b82ec7ce3f409dee7658a6575779a23db3795
Matt Kirby <31753757+mkirby1995@users.noreply.github.com>
2019-05-30 10:40:21 -0500

    Update __init__.py

--- explore/__init__.py
+++ explore/__init__.py
@@ -3,7 +3,9 @@
 data_explore - a collection of Data Science helper functions
 """
 
-import numpy as np
 import pandas as pd
-import matpliotlib.pyplot as plt
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 import sklearn
+from sklearn.metrics import classification_report, confusion_matrix
