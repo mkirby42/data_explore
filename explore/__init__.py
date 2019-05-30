6d6a0c65428c72437226b77090f59e24b8023ef5
Matt Kirby <31753757+mkirby1995@users.noreply.github.com>
2019-05-30 10:36:26 -0500

    Update and rename __init__ to __init__.py

---
+++ explore/__init__.py
@@ -0,0 +1,9 @@
+#usr/bin/env python
+"""
+data_explore - a collection of Data Science helper functions
+"""
+
+import numpy as np
+import pandas as pd
+import matpliotlib.pyplot as plt
+import sklearn
