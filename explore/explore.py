dc4e8bfb243cbf4f10f8fac5c9cd0d21d2ba0be0
Matt Kirby <31753757+mkirby1995@users.noreply.github.com>
2019-05-30 10:39:52 -0500

    Update explore.py

--- explore/explore.py
+++ explore/explore.py
@@ -7,6 +7,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import sklearn
 from sklearn.metrics import classification_report, confusion_matrix
 
 class explore:
